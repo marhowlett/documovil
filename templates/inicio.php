ccfb3c3d424ea0b93f1ae2ef97320bf6f0f12c16
marhowlett <marimar_r2003@hotmail.com>
2019-09-27 17:21:00 -0500

    .

--- templates/inicio.php
+++ templates/inicio.php
@@ -1,7 +1,40 @@
 <?php
-    /* Template Name: Inicio2 */ 
+    /* Template Name: Inicio */ 
     get_header();
 ?>
+<section id="banner" class="template-section">
+
+        
+<h1>¿No sabes <span>inglés?</span><b>¡NO TE PREOCUPES!</b></h1>
+           <div class="azul">
+            <h3>En DocuMóvil contamos con personal bilingüe <br>para apoyarte en todo tipo de trámites</h3>
+            </div>
+ <a href="#" class="btn">VER SOLUCIONES AL MIGRANTE</a>
+        
+      
+    </section>
+    <section id="quienes" class="template-section">
+
+        
+
+
+            <div class="grid">
+
+                <div class="col fondo">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/pavoreal.png" alt="Pavoreal">
+
+                </div>
+                <div class="col prueba_col">
+                  <h2>¿Quiénes somos?</h2>
+                    <p>Somos una red de especialistas dedicados a proporcionar servicios profesionales de documentación, trámites y orientación en diversas aéreas, mediante atención personalizada: en tu idioma y a precios accesibles, para el empoderamiento de la comunidad hispana en Arizona. </p>
+                 <a href="#" class="btn">Saber más</a>
+               
+                </div>
+
+
+            </div>
+      
+    </section>
 <?php 
     get_footer();
 ?>
