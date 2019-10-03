72847672f8bcf6404b5f51ab58c0399f55b66a64
marhowlett <marimar_r2003@hotmail.com>
2019-10-03 17:34:43 -0500

    .

--- templates/contacto.php
+++ templates/contacto.php
@@ -18,21 +18,18 @@
                 <div class="col fondo">
                    <p> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whatsapp.png" alt="Logo"> +1  480 253 7519</p>
                    <p> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/phone.png" alt="Logo"> +1  602 710 3885 </p>
-                   <p> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/location.png" alt="Logo"> Calle y número<br>
-Colonia , C.P<br>
-Municipio y Estado</p>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3774.2420962724414!2d-99.23410238578968!3d18.920675311754742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cddfad730d6307%3A0x87acd7a5bdab924e!2sFray+Bartolom%C3%A9+de+Las+Casas+15%2C+Cuernavaca+Centro%2C+Centro%2C+62000+Cuernavaca%2C+Mor.!5e0!3m2!1ses-419!2smx!4v1563573457546!5m2!1ses-419!2smx" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                    <div class="blanco">
+                   <p> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/location.png" alt="Logo"> P.O. Box 30181<br>
+Phoenix, AZ 85046 </p>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d831.9374802024122!2d-112.03068607079534!3d33.481861098797786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDI4JzU0LjciTiAxMTLCsDAxJzQ4LjUiVw!5e0!3m2!1ses-419!2smx!4v1570141340856!5m2!1ses-419!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                  <!--  <div class="blanco">
                         <p class="azul">Síguenos
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rede.png" alt="Logo">
                         
                         </p>
-                    </div>
+                    </div>-->
                 </div>
 
 
             </div>
 </section>
-<footer class="template-section">
-    <p>DocuMóvil®  Todos los derechos reservados.</p>
-</footer>
+
